55bebaa7385278affb8579efffe3e79afe344ebb
tomeullompartpiza <tomeullompartpiza@gmail.com>
2026-06-01 11:23:50 +0200

    - Settings app installation

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third parties
+    'rest_framework',
+    'corsheaders',
+    # Internal
+    'analytics',
 ]
 
 MIDDLEWARE = [
