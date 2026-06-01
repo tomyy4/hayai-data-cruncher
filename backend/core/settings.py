1397f0582cd924d4d331165149d55a38b5618edc
tomeullompartpiza <tomeullompartpiza@gmail.com>
2026-06-01 11:26:09 +0200

    Redis and postgres minimal config

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -55,6 +55,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Vite's default port
+    "http://127.0.0.1:5173",
+]
+
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
@@ -80,11 +85,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hayai_db',
+        'USER': 'hayai_user',
+        'PASSWORD': 'hayai_secure_pass',
+        'HOST': 'localhost',  # Points to Docker db service
+        'PORT': '5432',
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',  # Use first redis DB
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
